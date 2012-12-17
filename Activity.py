39c1f6fac3ba83cceb504562102a29ffaf7cd0a6
Fox C. Wilson <fox-wilson@Fox-Laptop.(none)>
2012-12-17 15:08:39 -0500

    Added a function which grabs all of the blocks available. Fun. Also, Git says that Authenticator was changed. I need to look at that.

--- Activity.py
+++ Activity.py
@@ -1,1 +1,39 @@
-class Activity
+from Iodine import *
+from BeautifulSoup import *
+class Activity:
+	"""
+	Activity defines an 8th period activity. It does this by taking a Block
+	as well as either an Activity ID or the name of an activity. Using the
+	activity ID will result in faster load times, however, the name will always
+	find the correct activity ID.
+	"""
+	def __init__(self, authObj, block, aid = None, name = None):
+		"""
+		Initialize the Activity. Pass in an Authenticator, Block, and either an
+		activity ID or an activity name.
+		"""
+		if aid is None and name is None: # If we don't get one of the arguments...
+			# Complain.
+			raise IodineException("Error: Not enough information was passed in to get the AID.", "ERR_NO_ARGS")
+		if not authObj.isAuthenticated: # If we aren't authenticated...
+			# Complain.
+			raise IodineException("Error: There is no usable Authenticator.", "ERR_NOT_AUTHED")
+		self.bid = block.bid # Get the Block's bid
+		if aid is not None: # If we got an AID...
+			self.aid = aid # Set it as a class variable
+			self.checkAid() # Do a sanity check, just to make sure it exists
+	def checkAid(self):
+		"""
+		Called internally.
+		"""
+		url = "https://iodine.tjhsst.edu/eighth/vcp_schedule/choose/uid/" + authObj.uid + "/bids/" + self.bid  # Construct a query URL
+		eighthList = ClientCookie.urlopen(url) # Open it
+		eighths = eighthList.read() # Read it
+		if self.aid not in eighths: # If the activity doesn't exist
+			# Complain
+			raise IodineException("Error: The activity ID passed in does not exist.", "ERR_NO_ACT")
+		else: # Don't complain
+			return True # Return True.
+		return False # This should never happen
+		
+		
--- Authenticator.py
+++ Authenticator.py
@@ -1,15 +1,51 @@
 import ClientCookie
+import urllib
 import urllib2
+from Iodine import *
 class Authenticator:
+	"""
+	Authenticator is a class which interfaces with the Iodine authentication
+	system. It submits an HTTPS POST request containing a username and
+	password. Iodine then returns a PHP session ID cookie, which the
+	ClientCookie module stores and passes to other components of the Iodine
+	API. Methods contained in this class are the __init__ method and the 
+	login method.
+	"""
 	def __init__(self, username, password):
-		self.username = username
-		self.password = password
+		"""
+		Initialize the Authenticator. Takes a username and password, which
+		are stored as class variables. This method does not actually log in
+		the username in question.
+		"""
+		self.username = username # Defines the username that the login method will use
+		self.password = password # Defines the password that the login method will use
+		self.isAuthenticated = False # Are we authenticated yet?
 	def login(self):
-		usernameKey = 'login_username'
-		passwordKey = 'login_password'
-		loginUrl = "https://iodine.tjhsst.edu"
-		loginInformation = {usernameKey: self.username, passwordKey: self.password}
-		loginRequest = urllib2.Request(loginUrl, loginInformation)
-		loginResponse = ClientCookie.urlopen(loginRequest)
-		return loginResponse
-
+		"""
+		Perform the actual login. This method takes the username and password
+		passed in when the class was initialized. It then creates a dictionary
+		with login information. This dictionary is passed into urllib2 to create
+		a Request, which is then passed to ClientCookie.urlopen. This method
+		returns a loginResponse, which is the source code from the default
+		Iodine module.
+		"""
+		try: # Just in case we're trying to run without an Internet connection or something
+			usernameKey = 'login_username' # Defines the username field name
+			passwordKey = 'login_password' # Defines the password field name
+			loginUrl = "https://iodine.tjhsst.edu" # Defines the URL that the request will use
+			loginInformation = {usernameKey: self.username, passwordKey: self.password} # Creates a request dictionary
+			loginInformation = urllib.urlencode(loginInformation) # Encode the login information.
+			loginRequest = urllib2.Request(loginUrl, loginInformation) # Creates a Request that is used to login
+			loginResponse = ClientCookie.urlopen(loginRequest) # Sends the login to Iodine and stores the PHP session ID.
+			loginResponse = loginResponse.read() # Get the HTML/XML from Iodine.
+			webpage = BeautifulSoup(loginResponse) # Set up a Beautiful Soup object
+			eighthChangeUrl = webpage.find(id="menu_eighth")['href'] # Grab the eighth period change URL
+			uid = eighthChangeUrl.split("uid/")[1] # Get the UID based on the eighth period change URL
+			self.uid = uid # And set the uid as a class variable, effectively getting the UID for changing things
+			self.isAuthenticated = True # Yes, yes we are logged in.
+			return True # Yay, no error!
+		except Exception, e: # If we failed for whatever reason...
+			self.uid = None # Set the uid to none.
+			self.isAuthenticated = False # No, no we are not.
+			print e
+			raise Exception("Error in Authenticator: could not log in.") # Raise an exception.
