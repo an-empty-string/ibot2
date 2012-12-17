225565b878ab14be82ee63f98efdfc1eab56dbce
Fox C. Wilson <fox-wilson@Fox-Laptop.(none)>
2012-12-17 15:29:04 -0500

    Once again added the getallblocks method in Block.

--- Block.py
+++ Block.py
@@ -56,3 +56,14 @@ class Block:
 			line = line.split("/activity/bid/")[1].split('">')[0]
 			# Store the bid
 			self.bid = line
+def getAllBids(authObj):
+	if authObj.isAuthenticated is not True:
+		raise IodineException("Not authenticated!", "ERR_NO_AUTH")
+	soup = BeautifulSoup(ClientCookie.urlopen("https://iodine.tjhsst.edu/eighth/vcp_attendance").read())
+	lines = soup.findAll('tr')
+	bids = []
+	for i in lines:
+		i = repr(i)
+		i.split("/activity/bid")[1].split('">')[0]
+		bids.append(i)
+	return bids
